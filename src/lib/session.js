b964d23292d7d7f394381eefd30f6ba84bdaeda5
Gamebot3 <ansh.jain1@outlook.com>
2019-01-26 17:40:18 -0600

    Picture Functions

--- src/lib/session.js
+++ src/lib/session.js
@@ -17,13 +17,13 @@ export const getAllTags = async () => {
     } catch(error) {
         Promise.reject(error)
     }
-})
+};
 
 /**
  * Adds a new class tag to storage
  * @param {integer/String} tag Tag with id and name
  */
-export const addNewClassTag = (tag) {
+export const addNewTag = (tag) => {
     return AsyncStorage.getItem('tags').then(item => {
         return JSON.parse(item);
     }).then(arr => {
@@ -32,5 +32,38 @@ export const addNewClassTag = (tag) {
         }
         arr.push(tag);
         return AsyncStorage.setItem('tags', JSON.stringify(arr));
+    });
+}
+
+/**
+ * Returns all of the pictures in the pictures part of the database
+ */
+export const getAllPictures = async () => {
+    let routes = [];
+    try {
+        let res = AsyncStorage.getItem('pictures');
+        pictures = JSON.parse(res);
+        if(tags) {
+            return Promise.resolve(pictures);
+        }
+        return Promise.reject();
+    } catch(error) {
+        Promise.reject(error)
+    }
+};
+
+/**
+ * Adds a new picture to storage
+ * @param {integer/String/DateTime/DateTime/String/integer} Picture Picture to add to storage
+ */
+export const addNewPicture = (picture) => {
+    return AsyncStorage.getItem('pictures').then(item => {
+        return JSON.parse(item);
+    }).then(arr => {
+        if(arr === null) {
+            arr = [];
+        }
+        arr.push(picture);
+        return AsyncStorage.setItem('pictures', JSON.stringify(arr));
     });
 }
