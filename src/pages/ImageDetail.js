e94aee4ac3e861a56847edd463c3ecb116744ce0
George Thayamkery <george.thayamkery@gmail.com>
2019-01-27 09:37:01 -0600

    these guys are flipped

--- src/pages/ImageDetail.js
+++ src/pages/ImageDetail.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View, Button, Image } from 'react-native'
+import { vw, vh } from 'react-native-expo-viewport-units';
+import { Spacing } from '../styles/theme';
 
 
 export class ImageDetail extends Component {
@@ -21,16 +23,19 @@ export class ImageDetail extends Component {
     const image = navigation.getParam('image', null);
     if(!image)
       return <View></View>
-
-    console.warn(image['path'])
-
+    
     return (
-      <View style={{backgroundColor: "#333", justifyContent: "center", alignItems: "center"}}>
+      <View style={{backgroundColor: "#000", justifyContent: "center", alignItems: "center"}}>
         <Image
           resizeMode = "contain"
-          style = {{ flex: 1 }}
+          style = {{ width: vw(100), height: vh(100) - 56}}
           source = {{ uri: image.path }}
         />
+        <View style={{position: 'absolute', bottom: 0, height: vh(20), width: vw(100), justifyContent: 'center', alignItems: 'center'}}>
+          <View style={{padding: Spacing.skinny, borderRadius: 16, backgroundColor: "rgba(0,0,0,0.4)"}}>
+            <Text style={{color: "#fff"}}>{new Date(image.dateCreated).toLocaleString()}</Text>
+          </View>
+        </View>
       </View>
     )
   }
