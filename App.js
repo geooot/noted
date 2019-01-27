74e552a89d4ada7ffe1b81ec0c3cf89f942960a1
Gamebot3 <ansh.jain1@outlook.com>
2019-01-26 18:40:06 -0600

    Image Functions

--- App.js
+++ App.js
@@ -51,6 +51,51 @@ const sideDrawerNavigator = createDrawerNavigator({
   drawerPosition: "left"
 })
 
+/**
+ * Function to retrive the GCP Text Detection for an image
+ * @param {String} imagePath File path for the image you want to classify
+ * @return {String} Returns the text representation of the supplied image
+ */
+function getTextFromImage(imagePath) {
+  return fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB2ryymZC_3vKF0vkQtZOH0aNiroY98ui8', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: {
+      "requests": [
+        {
+          "image": {
+            "content": imagePath
+          },
+          "features": [
+            {
+              "type": "TEXT_DETECTION"
+            }
+          ]
+        }
+      ]
+    }
+  }).catch((error) => {
+    console.error(error);
+  })
+}
 
+/**
+ * Function to only retrive the entire text dump from the entire GCP Response
+ * @param {String} gcpResponse Response from the Google Cloud Platform for the image text
+ * detection: Should be in a String format
+ * @return Only the relevant text dump property from the JSON 
+ */
+function getKeywords(gcpResponse){
+  let picObject = JSON.parse(gcpResponse);
+  try {
+    return picObject.textAnnotations.description;
+  } catch(error) {
+    console.error(error);
+    return error;
+  }
+}
 
 export default createAppContainer(sideDrawerNavigator);
